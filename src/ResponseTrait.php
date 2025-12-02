77930c8bbaf74777f9d80a3c1efbfa2ed0a8877e
fady reda <fady.r.andrawes@gmail.com>
2025-12-02 13:59:57 +0200

    add get pagination data function

--- src/ResponseTrait.php
+++ src/ResponseTrait.php
@@ -38,4 +38,22 @@ trait ResponseTrait
 
         return response()->json($payload, $code);
     }
+
+    protected function getPaginationData($data)
+    {
+        return [
+            'current_page' => $data->currentPage(),
+            'last_page' => $data->lastPage(),
+            'total_page' => $data->lastPage(),
+            'per_page' => $data->perPage(),
+            'total_items' => $data->total(),
+            'from' => $data->firstItem(),
+            'to' => $data->lastItem(),
+            'next_page_url' => $data->nextPageUrl(),
+            'prev_page_url' => $data->previousPageUrl(),
+            'first_page_url' => $data->url(1),
+            'last_page_url' => $data->url($data->lastPage()),
+            'path' => $data->path(),
+        ];
+    }
 }
